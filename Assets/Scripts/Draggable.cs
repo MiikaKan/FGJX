93f1196487ac39eb54abb9e201e638dcd53f6600
Unknown <heckmiika@hotmail.com>
2018-01-26 23:24:49 +0200

    Objects can be dragged and rotated

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour {
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    [SerializeField]
+    private int rotationAmount = 45;
+
+    private void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+
+        gameObject.transform.Rotate(new Vector3(0, rotationAmount, 0) * Input.GetAxis("Mouse ScrollWheel") * 10);
+
+        if (Input.GetKeyDown(KeyCode.Mouse2))
+        {
+            gameObject.transform.rotation = Quaternion.identity;
+        }
+    }
+}

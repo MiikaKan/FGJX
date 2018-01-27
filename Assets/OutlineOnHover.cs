2a30978b14a3fdeaa39094b2189e47ff8355b052
Oskari Liukku <oliukku@gmail.com>
2018-01-27 20:39:26 +0200

    took outline out of draggable, tweaked colors

---
+++ Assets/OutlineOnHover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+[RequireComponent(typeof(Outline))]
+public class OutlineOnHover : MonoBehaviour {
+
+    private Outline _outline;
+
+    private void Start()
+    {
+        _outline = GetComponent<Outline>();
+        _outline.enabled = false;
+    }
+
+    private void OnMouseEnter()
+    {
+        _outline.enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        _outline.enabled = false;
+    }
+}

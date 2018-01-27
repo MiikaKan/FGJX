e7abb1e0d201e1d71e7f402b488fdc61e9dc082e
Oskari Liukku <oliukku@gmail.com>
2018-01-27 18:10:02 +0200

    done button activates when signal reaches receiver

---
+++ Assets/LevelController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour {
+
+    [SerializeField]
+    private List<SignalReceiver> _requiredReceivers;
+    [SerializeField]
+    private DoneButton _doneButton;
+
+    private void Start()
+    {
+        foreach (var receiver in _requiredReceivers)
+        {
+            receiver.OnReceiveStatusChanged.AddListener(CheckCompletion);
+        }
+    }
+
+    private void CheckCompletion()
+    {
+        if (_requiredReceivers.TrueForAll(x => x.ReceivingStatus == true))
+        {
+            _doneButton.Activate();
+        }
+        else
+        {
+            _doneButton.Deactivate();
+        }
+    }
+
+}
--- Assets/Scripts/SignalReceiver.cs
+++ Assets/Scripts/SignalReceiver.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SignalReceiver : MonoBehaviour {
 
@@ -8,6 +9,9 @@ public class SignalReceiver : MonoBehaviour {
     private ScoreDisplay _scoreDisplay;
     private bool _receiving;
     private float _signalStrength;
+
+    public bool ReceivingStatus { get; private set; }
+    public UnityEvent OnReceiveStatusChanged = new UnityEvent();
 
 	public void Receive(float signalStrength)
     {
@@ -19,12 +23,22 @@ public class SignalReceiver : MonoBehaviour {
     {
         if (_receiving)
         {
-            print("Receiving signal of strength: " + _signalStrength);
+            //print("Receiving signal of strength: " + _signalStrength);
             _scoreDisplay.Score = _signalStrength;
         }
         else
         {
             _scoreDisplay.Score = 0f;
+        }
+
+        if(_receiving && ReceivingStatus == false)
+        {
+            ReceivingStatus = true;
+            OnReceiveStatusChanged.Invoke();
+        }else if(!_receiving && ReceivingStatus == true)
+        {
+            ReceivingStatus = false;
+            OnReceiveStatusChanged.Invoke();
         }
 
         _receiving = false;

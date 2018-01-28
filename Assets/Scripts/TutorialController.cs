ce8c89de098241198a9846da2736375d422a1885
Unknown <heckmiika@hotmail.com>
2018-01-28 15:07:57 +0200

    tutorialcontroller

---
+++ Assets/Scripts/TutorialController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _introPanel;
+
+    void Start()
+    {
+
+        LevelController levelController = (LevelController)FindObjectOfType(typeof(LevelController));
+        if (levelController.tutorialRead)
+        {
+            _introPanel.SetActive(false);
+        }
+        levelController.SetTutorialRead();
+    }
+
+}
